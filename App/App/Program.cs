ae1e1dc1b8c1f4cc4b31f144364f8c7d3f661ed0
HP <HP@DESKTOP-A20VHGG>
2025-09-16 12:56:54 +0530

    commit with db connection.

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -1,4 +1,7 @@
 
+using App.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace App
 {
     public class Program
@@ -7,6 +10,9 @@ namespace App
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                            options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
+            );
             // Add services to the container.
 
             builder.Services.AddControllers();
